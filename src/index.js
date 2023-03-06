b4188e58b76bc7d473737058f1096ab89a09852b
lcw3176 <lcw3176@naver.com>
2023-03-06 21:06:05 +0900

    refactor(index.js): 임시 로깅하던거 제거

--- src/index.js
+++ src/index.js
@@ -45,8 +45,6 @@ module.exports.handler = async (event, context) => {
 
   await page.setExtraHTTPHeaders(header);
 
-  console.log("페이지 요청");
-
   await page.goto(domain + `/image/template/` + companyType + `?`
     + `layerMode=` + layerMode
     + `&lat=` + lat
@@ -55,10 +53,8 @@ module.exports.handler = async (event, context) => {
     + `&type=` + type
     + `&companyType=` + companyType);
 
-  console.log("페이지 로딩 대기");
   await page.waitForSelector('#checker_true');
 
-  console.log("아이디 로딩 완료");
   let goal_width;
 
   switch (level) {
@@ -100,6 +96,7 @@ module.exports.handler = async (event, context) => {
         headers: header
       });
       
+
       let response = {
         "uuid": gen_uuid,
         "x": x,
@@ -112,7 +109,6 @@ module.exports.handler = async (event, context) => {
   }
 
   await browser.close();
-  console.log("브라우저 종료");
   
 
   return {
